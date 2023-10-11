50b896d64b8ebaaf89efb150aa47b319670926ec
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-10-11 22:29:37 +0200

    feat : clean code

--- dist/app.js
+++ dist/app.js
@@ -2,8 +2,9 @@
 const compteur = document.querySelector("#compteur");
 const span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector("span");
 let i = 0;
-const increment = (e) => [
-    i++,
-    span ? span.innerText = i.toString() : null
-];
+const increment = (e) => {
+    e.preventDefault();
+    i++;
+    span ? span.innerText = i.toString() : null;
+};
 compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener("click", increment);
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 const compteur = document.querySelector("#compteur")
 const span = compteur?.querySelector("span")
-
 let i : number = 0
 
-const increment = (e: Event) => [
-  i++,
-  
+const increment = (e: Event) => {
+  e.preventDefault()
+  i++;
  span ? span.innerText = i.toString() : null
-]
+}
 
 compteur?.addEventListener("click", increment)
