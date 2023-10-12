5619ccaf8ecf020bbfa68fb4470101d76b6ee7f3
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-10-12 23:29:26 +0200

    feat : add structure

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -2,7 +2,13 @@
 const App = () => {
   return (
     <div>
-      <h1>Hello world !</h1>
+      <h1>Poster un message</h1>
+      <form>
+        <input type="text" placeholder="Entrez un message" id="inputMessage" />
+        <input type="submit" value="Envoyer"></input>
+      </form>
+      <h1>Liste des messages</h1>
+      <div></div>
     </div>
   );
 };
