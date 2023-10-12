2ea3d6c269e976aae3cb29376535183506d8afef
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-10-12 23:44:40 +0200

    feat : add onSubmit + handleSubmit

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,12 +1,18 @@
 import { useRef } from "react";
 
 const App = () => {
-const inputMessage = useRef<HTMLInputElement>(null)
+  const inputMessage : any = useRef<HTMLInputElement>(null)
+  
+  const handleSubmit  =  (e : any) => {
+    e.preventDefault()
+
+   inputMessage.current.value=""
+  }
 
   return (
     <div>
       <h1>Poster un message</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Entrez un message" id="inputMessage" ref={inputMessage} />
         <input type="submit" value="Envoyer"></input>
       </form>
