378ce2f38bfa282496cdd737fcded70d7c579397
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-10-12 23:34:18 +0200

    feat : add useRef

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,10 +1,13 @@
+import { useRef } from "react";
 
 const App = () => {
+const inputMessage = useRef<HTMLInputElement>(null)
+
   return (
     <div>
       <h1>Poster un message</h1>
       <form>
-        <input type="text" placeholder="Entrez un message" id="inputMessage" />
+        <input type="text" placeholder="Entrez un message" id="inputMessage" ref={inputMessage} />
         <input type="submit" value="Envoyer"></input>
       </form>
       <h1>Liste des messages</h1>
